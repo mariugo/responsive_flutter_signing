e78dea4cb4c189f29c356905cefe76bb185eb5ae
Mariugo Megiani <mariugomegiani@gmail.com>
2022-12-27 19:29:11 -0300

    Created Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_signing/app/constants/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Responsive Signing',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: null);
+      title: 'Responsive Signing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
   }
 }
