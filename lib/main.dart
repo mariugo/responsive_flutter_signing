b2b9c25426a5a629a65eb7825ee37ac8480db34e
Mariugo Megiani <mariugomegiani@gmail.com>
2022-12-27 19:31:54 -0300

    Changes to theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_signing/app/constants/appconstants.dart';
 import 'package:responsive_signing/app/constants/pages/login_page.dart';
 
 void main() {
@@ -12,8 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Responsive Signing',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.backgroundColor,
       ),
       home: const LoginPage(),
     );
