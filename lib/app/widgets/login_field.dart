087b14362c80ef928d19a71480e4aebe8c912c80
Mariugo Megiani <mariugomegiani@gmail.com>
2022-12-31 14:08:07 -0300

    LoginField done

---
+++ lib/app/widgets/login_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:responsive_signing/app/constants/appconstants.dart';
+
+class LoginField extends StatelessWidget {
+  final String hintText;
+  const LoginField({
+    Key? key,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxWidth: 400,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          contentPadding: const EdgeInsets.all(27),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: AppColors.borderColor,
+              width: 3,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: AppColors.gradient2,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
