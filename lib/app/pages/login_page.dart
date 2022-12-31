d75220dd874c002239a9458f1abc306bb0a8f31d
Mariugo Megiani <mariugomegiani@gmail.com>
2022-12-31 13:24:03 -0300

    Added SafeArea

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -7,38 +7,41 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset('assets/images/signin_balls.png'),
-              const Text(
-                'Entrar.',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset('assets/images/signin_balls.png'),
+                const Text(
+                  'Entrar.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              const SocialButtonWidget(
+                const SizedBox(height: 50),
+                const SocialButtonWidget(
                   imagePath: 'assets/svg/g_logo.svg',
-                  label: 'Entrar com Google'),
-              const SizedBox(
-                height: 20,
-              ),
-              const SocialButtonWidget(
-                imagePath: 'assets/svg/f_logo.svg',
-                label: 'Entrar com Facebook',
-                horizontalPadding: 90,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Ou',
-                style: TextStyle(fontSize: 17),
-              ),
-            ],
+                  label: 'Entrar com Google',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const SocialButtonWidget(
+                  imagePath: 'assets/svg/f_logo.svg',
+                  label: 'Entrar com Facebook',
+                  horizontalPadding: 90,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Ou',
+                  style: TextStyle(fontSize: 17),
+                ),
+              ],
+            ),
           ),
         ),
       ),
