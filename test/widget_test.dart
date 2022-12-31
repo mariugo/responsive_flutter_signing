13fa2596e8c7a6affafd366df613b0f1d4b9b5ad
Mariugo Megiani <mariugomegiani@gmail.com>
2022-12-31 14:07:25 -0300

    initial widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,19 @@
+import 'dart:math';
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:responsive_signing/main.dart';
+
+void main() {
+  testWidgets('Find Enter Text', (WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const MyApp());
+    expect(find.text('Entrar.'), findsOneWidget);
+  });
+  testWidgets('Find Enter with Google', (WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const MyApp());
+    expect(find.text('Entrar com Google'), findsOneWidget);
+  });
+  testWidgets('Find Enter with Facebook', (WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const MyApp());
+    expect(find.text('Entrar com Facebook'), findsOneWidget);
+  });
+}
